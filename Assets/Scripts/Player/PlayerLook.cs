4276eff1283eabec0a5c71090ef5ffca8a7cf234
aydenlimyip <65572042+aydenlimyip@users.noreply.github.com>
2022-05-06 14:33:21 +1200

    Player Movement and Player Look Done

---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] new private Camera camera;
+    // current rotation of camera
+    private float spin = 0.0f;
+    private float tilt = 0.0f;
+
+    private float targetCameraPos;
+
+    [SerializeField] private Vector2 tiltExtents = new Vector2(-85.0f, 85.0f);
+    [SerializeField] private Vector2 spinExtents;
+    [SerializeField] private float sensitivity = 2.0f;
+    bool cursorLocked = false;
+
+    // getter to get the camera
+    public Camera GetCamera() { return camera; }
+    // getter to get the spin value
+    public float GetSpin() { return spin; }
+    // setter to set the spin value
+    public void SetSpin(float newSpin) { spin = newSpin; }
+    // getter to get the sensitivity value
+    public float GetSensitivity() { return sensitivity; }
+    // setter to set the sensitivity value
+    public void SetSensitivity(float _sensitivity) { sensitivity = _sensitivity; }
+
+    public void LockCursor()
+    {
+        cursorLocked = !cursorLocked;
+        // locks cursor in the middle of screen
+        Cursor.lockState = cursorLocked ? CursorLockMode.Locked : CursorLockMode.None;
+        // cursor is not visable
+        Cursor.visible = !cursorLocked;
+    }
+
+    private void HandleEditorInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            LockCursor();
+        }
+    }
+    private void MoveCamera()
+    {
+        // if cursor is locked
+        if (cursorLocked)
+        {
+            //getting input for the x and y axis for the mouse
+            // using GetAxisRaw so that we are dealing with 0 and 1
+            float mouseX = Input.GetAxisRaw("Mouse X");
+            float mouseY = Input.GetAxisRaw("Mouse Y");
+
+            // moves the players look when moving the mouse
+            spin += mouseX * sensitivity;
+            tilt -= mouseY * sensitivity;
+
+            // stops the player from snapping their neck
+            tilt = Mathf.Clamp(tilt, tiltExtents.x, tiltExtents.y);
+
+            // rotation on the x axis for the mouse (rotating head to look from side to side)
+            // rotation on the y axis for the mouse (rotating head so looking up and down)
+            camera.transform.localEulerAngles = new Vector3(tilt, spin, 0);
+            camera.transform.localPosition += Quaternion.Euler(0.0f, spin, 0.0f) * new Vector3(targetCameraPos, 0, 0);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        LockCursor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+        HandleEditorInputs();
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerLook lookScript;
+    private CharacterController cController;
+
+    [Header("General Movement Values")]
+    [SerializeField]
+    private float moveSpeed = 12.0f;
+    [SerializeField]
+    private float gravity = 30.0f;
+    [SerializeField]
+    private float jumpSpeed = 15.0f;
+    [SerializeField]
+    private float airStrafeSpeed = 1.0f;
+    private float acceleration = 10.0f;
+    private AnimationCurve frictionCurve = AnimationCurve.Linear(0, 0.1f, 1, 1);
+    [SerializeField]
+    private float coyoteTime = 0.75f;
+    private float currentCoyoteTime;
+
+    [Header("Character velocity")]
+    private Vector3 velocity;
+    private Vector3 StoredJumpVelo;
+
+    [Header("Checks")]
+    private bool isGrounded = false;
+
+    [Header("Head Bobbing")]
+    private float headBobTimer = 0.0f;
+    private float headBobFrequency = 1.0f;
+    private float headBobAmplitude = 0.1f;
+    // the default position of the head
+    private float headBobNeutral = 1.0f;
+    private float headBobMinSpeed = 0.1f;
+    private float headBobBlendSpeed = 4.0f;
+    [SerializeField] private AnimationCurve headBobBlendCurve = AnimationCurve.EaseInOut(0.0f, 0.0f, 1.0f, 1.0f);
+    private float headBobMultiplier = 0.0f;
+    private Vector3 oldPos;
+    private Vector3 newPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookScript = this.gameObject.GetComponent<PlayerLook>();
+        cController = this.gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoving();
+    }
+
+    private void PlayerMoving()
+    {
+        // reads players input 
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Jumping();
+        HeadBobbing();
+
+        // converting the players input into a vector 3 and timings it by the players look direction
+        Vector3 inputMove = new Vector3(x, 0.0f, z);
+        Vector3 realMove = Quaternion.Euler(0.0f, lookScript.GetSpin(), 0.0f) * inputMove;
+        realMove.Normalize();
+
+        // friction
+        // we store the y velocity
+        float cacheY = velocity.y;
+        // set the y velocity to be 0
+        velocity.y = 0;
+        // movement for the player
+        velocity += realMove * acceleration * Time.deltaTime;
+        // friction for the players x and z axis
+        velocity -= velocity.normalized * acceleration * frictionCurve.Evaluate(velocity.magnitude) * Time.deltaTime;
+        // we give back the y velocity
+        velocity.y = cacheY;
+
+        // multiplies velocity by desired movespeed
+        float planarSpeed = moveSpeed;
+        velocity.x = realMove.x * planarSpeed;
+        velocity.z = realMove.z * planarSpeed;
+
+        // gravity on the player
+        velocity.y -= gravity * Time.deltaTime;
+        // getting the position before the player moves (headbobbing)
+        oldPos = transform.position;
+        // moving the player on screen
+        cController.Move(velocity * Time.deltaTime);
+        // getting the position after the player moves (headbobbing)
+        newPos = transform.position;
+
+        // collision when touching the roof
+        if((cController.collisionFlags & CollisionFlags.Above) != 0)
+        {
+            velocity.y = -1.0f;
+        }
+
+        CoyoteTime();
+    }
+
+    private void Jumping()
+    {
+        // checks if player is on the ground and if player has press space
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = jumpSpeed;
+            isGrounded = false;
+        }
+    }
+
+    private void CoyoteTime()
+    {
+        // collision detection for player
+        if((cController.collisionFlags & CollisionFlags.Below) != 0)
+        {
+            isGrounded = true;
+            StoredJumpVelo = velocity;
+            velocity.y = -1.0f;
+            currentCoyoteTime = coyoteTime;
+        }
+        // coyoteTime
+        if (currentCoyoteTime > 0)
+        {
+            currentCoyoteTime -= Time.deltaTime;
+        }
+        else if (currentCoyoteTime < 0)
+        {
+            isGrounded = false;
+            currentCoyoteTime = coyoteTime;
+        }
+    }
+
+    private void HeadBobbing()
+    {
+        // getting the difference between the oldpos and newpos
+        Vector3 frameMove = newPos - oldPos;
+        Vector2 planarFrameMove = new Vector2(frameMove.x, frameMove.z);
+        headBobTimer += planarFrameMove.magnitude;
+
+        // to get how much the head moves per frame
+        Vector2 planarFrameVelocity = planarFrameMove;
+        planarFrameVelocity.x /= Time.deltaTime;
+        planarFrameVelocity.y /= Time.deltaTime;
+
+        // for blending back to the neutral position of the head
+        if (isGrounded && planarFrameVelocity.magnitude > headBobMinSpeed)
+        {
+            if (headBobMultiplier <= 0.0f)
+            {
+                headBobMultiplier = 1.0f;
+            }
+            else
+            {
+                headBobMultiplier += headBobBlendSpeed * Time.deltaTime;
+                headBobMultiplier = Mathf.Min(1.0f, headBobMultiplier);
+            }
+        }
+        else
+        {
+            headBobMultiplier -= headBobBlendSpeed * Time.deltaTime;
+            headBobMultiplier = Mathf.Max(0.0f, headBobMultiplier);
+
+            if (headBobMultiplier <= 0.0f)
+            {
+                headBobTimer = 0.0f;
+            }
+        }
+
+        // movement for the head bobbing
+        Vector3 localPos = lookScript.GetCamera().transform.localPosition;
+        float headBobMove = (Mathf.Cos(headBobTimer * headBobFrequency) - 1.0f) * headBobAmplitude;
+        localPos.y = headBobNeutral + (headBobMove * headBobBlendCurve.Evaluate(headBobMultiplier));
+        lookScript.GetCamera().transform.localPosition = localPos;
+    }
+}
+

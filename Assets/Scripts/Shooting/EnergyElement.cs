c144433fb1693990ef487837944fb3e896f8f232
RowanMoss <68680289+RowanMoss@users.noreply.github.com>
2022-05-09 10:17:21 +1200

    Energy script

---
+++ Assets/Scripts/Shooting/EnergyElement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyElement : BaseElementClass
+{
+    float fullRestoreAmount;
+    float perTickRestore;
+
+    private void Start()
+    {
+        heldCast = true;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Mouse1) & playerHand.GetCurrentAnimatorStateInfo(0).IsName(""))
+        {
+
+        }
+    }
+
+    public override void ElementEffect()
+    {
+        base.ElementEffect();
+        pData.mana += fullRestoreAmount;
+        if(pData.mana > pData.maxMana)
+        {
+            pData.mana = pData.maxMana;
+        }
+
+    }
+
+    protected override void StartAnims(string animationName)
+    {
+        base.StartAnims(animationName);
+
+        playerHand.SetTrigger(animationName);
+
+
+    }
+}

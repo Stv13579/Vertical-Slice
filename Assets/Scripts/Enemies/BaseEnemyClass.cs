61f90c3c5f0ec5b45ff7208635914afa6d6a4c51
RowanMoss <68680289+RowanMoss@users.noreply.github.com>
2022-05-09 21:03:06 +1200

    Base enemy class and files moved

--- Assets/Scripts/Enemies/BaseEnemyClass.cs
+++ Assets/Scripts/Enemies/BaseEnemyClass.cs
@@ -6,14 +6,67 @@ public class BaseEnemyClass : MonoBehaviour
 {
     //base class that all enemies derive from.
 
+    [SerializeField]
+    EnemyData eData;
 
+    GameObject player;
+
+    float startY;
+
+    private void Start()
+    {
+        startY = transform.position.y;
+        player = GameObject.Find("Player");
+    }
+
+    public void Update()
+    {
+        Movement(player.transform.position);
+        Attacking();
+    }
 
     //Movement
+    public void Movement(Vector3 positionToMoveTo)
+    {
+        //if((positionToMoveTo - transform.position).magnitude > eData.maxHopDistance & (eData.currentHopDistance == eData.maxHopDistance))
+        //{
+        //    //Move instead
+        //}
+        //else
+        //{   
+            
+
+        //}
+
+        //Come back to hopping
+        transform.position += (positionToMoveTo - transform.position).normalized * eData.moveSpeed * Time.deltaTime;
+    }
+
 
     //Attacking
+    public virtual void Attacking()
+    {
+
+    }
+
 
     //Taking damage
+    public void TakeDamage(float damageToTake)
+    {
+        eData.currentHealth -= damageToTake * eData.damageResistance - eData.damageThreshold;
+        Death();
+    }
 
-
- 
+    //Death
+    public void Death()
+    {
+        if(eData.currentHealth <= 0)
+        {
+            eData.dead = true;
+            //Normally do death animation/vfx, might even fade alpha w/e before deleting.
+            //Destroy for now
+            Destroy(gameObject);
+        }
+    }
+    
 }
--- Assets/Scripts/ScriptableObjects/EnemyData.cs
+++ Assets/Scripts/ScriptableObjects/EnemyData.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyData : MonoBehaviour
+public class EnemyData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float maxHealth;
+    public float currentHealth;
+    public float damageAmount;
+    public float moveSpeed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //The amount of flat damage any instance of incoming damage is reduced by
+    public float damageThreshold;
+
+    //The amount of percentage damage any instance of incoming damage is reduced by
+    public float damageResistance = 1;
+
+    public bool dead = false;
+
+    public float maxHopDistance;
+    public float currentHopDistance;
+
+    public AnimationCurve slimeHopHeight;
+
+
+
 }

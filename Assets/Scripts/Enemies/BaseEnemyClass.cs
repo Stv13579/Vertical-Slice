e7d61c8ed1a7854ed16ac858ffcdb058ec8a652f
RowanMoss <68680289+RowanMoss@users.noreply.github.com>
2022-05-09 20:17:12 +1200

    Enemy Data class and base class declared

---
+++ Assets/Scripts/Enemies/BaseEnemyClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemyClass : MonoBehaviour
+{
+    //base class that all enemies derive from.
+
+
+
+    //Movement
+
+    //Attacking
+
+    //Taking damage
+
+
+ 
+}

c925008d93924ae655aea65074574dc30f562284
Seb <sebastian@thevalentines.net>
2022-05-15 11:52:53 +1200

    Acid cloud implemented, testing UI done, minor changes to support UI

---
+++ Assets/Scripts/Shooting/AcidCloudElement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidCloudElement : BaseElementClass
+{
+    //Creates a cloud of acid, which deals damage over time to enemies within the cloud
+
+    [SerializeField]
+    GameObject cloudProj;
+
+    public float damage;
+
+    public float cloudSize;
+
+    public float cloudDuration;
+
+    void Update()
+    {
+        base.Update();
+    }
+    public override void ElementEffect()
+    {
+        base.ElementEffect();
+        //
+        Vector3 camLook = Camera.main.transform.forward;
+        camLook = new Vector3(camLook.x, 0.0f, camLook.z).normalized;
+        GameObject newShard = Instantiate(cloudProj, transform.position + (camLook * 3), Quaternion.identity);
+        newShard.GetComponent<AcidCloud>().SetVars(damage, cloudSize, cloudDuration);
+    }
+
+    public override void ActivateVFX()
+    {
+        base.ActivateVFX();
+
+
+    }
+
+    protected override void StartAnims(string animationName)
+    {
+        base.StartAnims(animationName);
+
+        playerHand.SetTrigger(animationName);
+
+    }
+}
---
+++ Assets/Scripts/UI/UITestScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITestScript : MonoBehaviour
+{
+    public PlayerData pData;
+    Shooting player;
+
+    [SerializeField]
+    Text manaText, healthText, leftSpellText, rightSpellText, comboSpellText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Shooting>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        manaText.text = "Mana: " + pData.mana + "/" + pData.maxMana;
+        healthText.text = "Health: " + pData.health + "/" + pData.maxHealth;
+        leftSpellText.text = "Left Spell: " +  player.GetPrimaryElement();
+        rightSpellText.text = "Right Spell: " + player.GetCatalystElement();
+        comboSpellText.text = "Combo Spell: " + player.GetComboElement();
+
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            leftSpellText.enabled = !leftSpellText.enabled;
+            rightSpellText.enabled = !rightSpellText.enabled;
+            comboSpellText.enabled = !comboSpellText.enabled;
+
+        }
+    }
+}

adbb473a486178a046bbdc5aaea39e69ec6d7b1e
aydenlimyip <65572042+aydenlimyip@users.noreply.github.com>
2022-05-18 17:40:04 +1200

    Decal and Fire Trial Fixed

--- Assets/Scripts/Enemies/FireSlimeEnemy.cs
+++ Assets/Scripts/Enemies/FireSlimeEnemy.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class FireSlimeEnemy : BaseEnemyClass
 {
     public GameObject enemyTrail;
+    [SerializeField]
+    private LayerMask trailLayerMask;
+    float trailCastDistance = 1.0f;
+    float backCastDistance = 0.1f;
+    float trailOffset = 0.01f;
     new private void Update()
     {
         Movement(player.transform.position);
@@ -13,10 +18,19 @@ public class FireSlimeEnemy : BaseEnemyClass
     public override void Attacking()
     {
         base.Attacking();
-        // creates a plane which is the trail of the fire slime
-        GameObject tempEnemyTrail = Instantiate(enemyTrail, transform.position + new Vector3(0.0f, 0.1f, 0.0f), Quaternion.identity);
-        // sets the damage
-        tempEnemyTrail.GetComponent<FireSlimeTrail>().SetVars(eData.damageAmount);
+        RaycastHit hit;
+        Vector3 Back = new Vector3(0f, backCastDistance, 0f);
+        // check of the ray cast line is hitting the ground
+        if (Physics.Raycast(transform.position + Back, -transform.up, out hit, trailCastDistance + backCastDistance, trailLayerMask))
+        {
+            Debug.Log(hit.transform.gameObject.name);
+            // rotate the go if it hits a flat surface or an angled surface
+            Quaternion newrotation = Quaternion.FromToRotation(transform.up, hit.normal);
+            // creates a plane which is the trail of the fire slime
+            GameObject tempEnemyTrail = Instantiate(enemyTrail, hit.point + hit.normal * trailOffset, newrotation);
+            // sets the damage
+            tempEnemyTrail.GetComponent<FireSlimeTrail>().SetVars(eData.damageAmount);
+        }
     }
 
     public override void Movement(Vector3 positionToMoveTo)
@@ -33,8 +47,8 @@ public class FireSlimeEnemy : BaseEnemyClass
         if (GetComponent<Rigidbody>().velocity.y < 10)
         {
             GetComponent<Rigidbody>().AddForce(0, 100, 0);
-            Attacking();
         }
+        Attacking();
     }
 
     private void OnCollisionStay(Collision collision)

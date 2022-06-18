9ade6dddaeed40e910e86c77ba374cc864897d35
aydenlimyip <65572042+aydenlimyip@users.noreply.github.com>
2022-06-19 00:18:32 +1200

    sound for menu

--- Assets/Scripts/UI/ExitGameController.cs
+++ Assets/Scripts/UI/ExitGameController.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class ExitGameController : MonoBehaviour
 {
+    AudioManager audioManager;
+    public void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
     public void EndGame()
     {
+        audioManager.Stop("Menu and Pause");
+        audioManager.Play("Menu and Pause");
         Application.Quit();
     }
 }
--- Assets/Scripts/UI/StartGameController.cs
+++ Assets/Scripts/UI/StartGameController.cs
@@ -5,8 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class StartGameController : MonoBehaviour
 {
+    AudioManager audioManager;
+    public void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
     public void StartGame()
     {
+        audioManager.Stop("Menu and Pause");
+        audioManager.Play("Menu and Pause");
         SceneManager.LoadScene(1);
     }
 }
